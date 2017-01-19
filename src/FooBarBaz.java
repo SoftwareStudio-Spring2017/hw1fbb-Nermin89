1b7a8ae37102f8b0bb9a09382fcffd821b0d8258
Nermin89 <n10000word@hotmail.com>
2017-01-19 12:46:39 -0500

    The Game

--- src/FooBarBaz.java
+++ src/FooBarBaz.java
@@ -1,9 +1,32 @@
 
+
 public class FooBarBaz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		PrintFooBarGame();
+		
 	}
 
+	public static void PrintFooBarGame(){
+		
+		for(int i=1; i <=50; i++){
+			//If divisible by 3 and 5, say “Foo Bar”
+			if(i%3==0 && i%5==0){
+				System.out.println("Foo Bar");
+			}
+			//If your number is divisible by 3, say “Foo” 
+			if(i%3==0){
+				System.out.println("Foo");
+			}
+			////If your number is divisible by 5, say “Bar” 
+			else if(i%5==0){
+				System.out.println("Bar");
+			}
+			 
+			else {
+				System.out.println(i);
+			}
+		}
+	}
 }
